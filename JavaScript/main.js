8f0dbb3f291d05945f8880965937a41d4daae2e7
KennyLeg <caleberlansanchez@gmail.com>
2024-11-19 14:23:31 -0400

    o

---
+++ JavaScript/main.js
@@ -0,0 +1,36 @@
+function Sumar(){
+let numero1=9;
+let numero2=4;
+let numero3=2;
+let resultado=numero1+numero2+numero3;
+alert("la suma es " + resultado);
+
+}
+function Restar(){
+let numero1=9;
+let numero2=4;
+let numero3=2;
+let resultado=numero1-numero2-numero3;
+alert("la resta es " + resultado);
+
+}
+
+function Multiplicar(){
+let numero1=9;
+let numero2=4;
+let numero3=2;
+let resultado=numero1*numero2*numero3;
+alert("la multiplicacion es " + resultado);
+
+}
+
+function Dividir(){
+let numero1=9;
+let numero2=4;
+let numero3=2;
+let resultado=numero1/numero2/numero3;
+alert("la division es " + resultado);
+
+}
+
+
